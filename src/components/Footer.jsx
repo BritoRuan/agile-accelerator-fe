84f05542fd6093ec8c3a80eeb3e98fdda287e72c
Ruan Brito <ruan.desenvolvedor@gmail.com>
2025-01-08 14:41:50 -0300

    feat: added footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <>
+      <footer className="bg-blue-500 text-white p-4 text-center">
+        <p>© 2024 Galeria de Fotos. Todos os direitos reservados</p>
+      </footer>
+    </>
+  );
+}
