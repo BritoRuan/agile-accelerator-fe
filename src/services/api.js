f4c668efcda430241918e59a028c312d8c9fbe51
Ruan Brito <ruan.desenvolvedor@gmail.com>
2025-01-08 21:05:48 -0300

    refactor: updated api file

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 export async function getImages(query) {
+  const API_KEY = "48111710-9796b8cff12b655de682c1d1b";
+
   const imagesData = await fetch(
-    `https://pixabay.com/api/?key=48111710-9796b8cff12b655de682c1d1b&q=${query}&per_page=10`
+    `https://pixabay.com/api/?key=${API_KEY}&q=${query}&per_page=10`
   );
   const images = imagesData.json();
   return images;
