61f38f0fa614fdec5dbbb61e4db7444b98d234ee
Ruan Brito <ruan.desenvolvedor@gmail.com>
2025-04-10 11:21:56 -0300

    refactor: import environment variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 export async function getImages(query) {
-  const API_KEY = "48111710-9796b8cff12b655de682c1d1b";
+  const API_KEY = import.meta.env.VITE_API_KEY;
 
   const imagesData = await fetch(
     `https://pixabay.com/api/?key=${API_KEY}&q=${query}&per_page=10`
